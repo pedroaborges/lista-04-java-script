2f7f19f447c12effd5a9f0bc35b1137084939c60
Pedro Antonio <pedrodev81@gmail.com>
2026-03-20 02:07:10 -0300

    addcss

--- exercicio01/scripts.js
+++ exercicio01/scripts.js
@@ -1,8 +1,18 @@
 let titulo1 = document.querySelector("#titulo1");
 let paragrafo1 = document.querySelector("#paragrafo1");
 let btn1 = document.querySelector("#button1");
+let mudou = false;
 
 btn1.onclick = function(){
-    titulo1.textContent = "TROQUEI";
-    paragrafo1.textContent = "TROCANDO";
+    if(!mudou){
+        titulo1.textContent = "TROQUEI";
+        paragrafo1.textContent = "TROCANDO";
+        mudou = true;
+    } else {
+        titulo1.textContent = "IFTM";
+        paragrafo1.textContent = "CAMPUS ITUIUTABA";
+        mudou = false;
+    }
+    
 }
+
